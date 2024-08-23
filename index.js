0b967bf3b7431963f849347f3892f7f4b5427db5
Reece Peardon <peardon.reece@gmail.com>
2024-08-22 20:42:05 -0700

    added api functions

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 //api "https://pokeapi.co/api/v2/pokemon/"
+
+
 async function main() {
-    const name = await fetch("https://pokeapi.co/api/v2/pokemon/name")
-    const nameData = await URLSearchParams.json
+    const mon = await fetch("https://digimon-api.vercel.app/api/digimon")
+    const monData = await mon.json()
+    const monListEl = document.querySelector('.mon__list')
+    monListEl.innerHTML = monData.map((name) =>monHTML(name)).join('')
+}
+
+main()
+
+function monHTML(name) {
+    return `<div class="card__wrapper">
+        <div class="pokemon-card">
+            <h3>${name.name}</h3>
+            <figure class="sprite">
+                <img src="${name.img}" alt="">
+            </figure>
+            <p class="level">${name.level}</p>
+        </div>
+    </div>`
+}
+
+function onSearchChange(event) {
+    console.log(event)
 }
