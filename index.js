62394d016b5b2e3d6ce2ef2b5633370d826c92c1
Reece Peardon <peardon.reece@gmail.com>
2024-08-29 21:06:40 -0700

    adding search bar functions

--- index.js
+++ index.js
@@ -5,44 +5,32 @@ async function main() {
     const mon = await fetch("https://digimon-api.vercel.app/api/digimon")
     const monData = await mon.json()
     const monListEl = document.querySelector('.mon__list')
-    monListEl.innerHTML = monData.map((name) =>monHTML(name)).join('')
+    const searchInput = document.getElementById('searchInput')
     
     window.sortListByName = function(event) {
-    monData.sort((a, b) => a.name.localeCompare(b.name))
-    monListEl.innerHTML = monData.map((name) =>monHTML(name)).join('')
+        monData.sort((a, b) => a.name.localeCompare(b.name))
+        monHTML()
     }
     
     window.sortListByLevel = function(event) {
-    monData.sort((a, b) => a.level.localeCompare(b.level))
-    monListEl.innerHTML = monData.map((name) =>monHTML(name)).join('')
+        monData.sort((a, b) => a.level.localeCompare(b.level))
+        monHTML()
     }
 
-    /*const searchInput = document.getElementById('searchInput')
-    searchInput.addEventListener('keyup', e => {
-        const searchString = e.target.value.toLowerCase()
-        const filteredMon = monData.filter(name => {
-            return (
-                name.name.toLowerCase().includes(searchString)
-            )
+    window.filterResults = function(event) {
+        monData.filter(filter)
+        monHTML
+    }
+
+    function filter() {
+        searchInput.addEventListener('input', () => {
+            const query = searchInput.value.trim()
         })
-        monListEl.innerHTML = monData.map((filteredMon) =>
-            `<div class="card__wrapper">
-        <div class="pokemon-card">
-            <h3>${filteredMon.name}</h3>
-            <figure class="sprite">
-                <img src="${filteredMon.img}" alt="">
-            </figure>
-            <p class="level">${filteredMon.level}</p>
-        </div>
-    </div>`)
-    })*/
-}
+    }
 
-
-main()
-
-function monHTML(name) {
-    return `<div class="card__wrapper">
+    function monHTML(name) {
+        monListEl.innerHTML = ''
+        monListEl.innerHTML = monData.map((name) =>`<div class="card__wrapper">
         <div class="pokemon-card">
             <h3>${name.name}</h3>
             <figure class="sprite">
@@ -50,5 +38,11 @@ function monHTML(name) {
             </figure>
             <p class="level">${name.level}</p>
         </div>
-    </div>`
+    </div>`).join('')
+    }
 }
+
+
+main()
+
+
